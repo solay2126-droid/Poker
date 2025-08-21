a3b92d7ca2229601f3b4a57a86307697ad1fb0f2
solay2126-droid <solay2126@gmail.com>
2025-08-21 12:27:45 +0200

    Update PlayerPanel.tsx

--- src/components/PlayerPanel.tsx
+++ src/components/PlayerPanel.tsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { TextField, Paper, Typography } from "@mui/material";
 
+type Props = {
+  playerName: string;
+  setPlayerName: (v: string) => void;
+};
+
+export default function PlayerPanel({ playerName, setPlayerName }: Props) {
+  return (
+    <Paper sx={{ p: 2, mt: 2 }}>
+      <Typography variant="h6">Nickname</Typography>
+      <TextField
+        fullWidth
+        placeholder="Scegli un nickname"
+        value={playerName}
+        onChange={(e) => setPlayerName(e.target.value)}
+        sx={{ mt: 1 }}
+      />
+    </Paper>
+  );
+}
